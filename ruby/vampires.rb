8ac9d15b676c56317e0ef1266de0946345c7bafb
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-05-24 19:22:56 -0700

    add case to select vampire_status

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -27,7 +27,6 @@ else
 	correct_age = false
 end
 
-
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
 
@@ -35,34 +34,52 @@ puts "Would you like to enroll in the company's health insurance? (y/n)"
 health_insurance = gets.chomp
 
 
-# creating vampire detector method
+
+# Condition 1: If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 
 if correct_age == true && (garlic_bread == "y" || health_insurance == "y")
-	vampire_result = "prob_not"
-elsif correct_age == false && (garlic_bread == "n" || health_insurance == "n")
-	vampire_result = "prob"
-elsif correct_age == false && garlic_bread == "n" && health_insurance == "n"
-	vampire_result = "almost_certainly"
-elsif vampire_name == true
-	vampire_result = "definitely"
+	vampire_status = "probably not"
 else
-	vampire_result = "inconclusive"
+	#do nothing
 end
 
-def vampire_detector(vampire_result)
-	if vampire_result == "definitely"
-		puts "Definitely a vampire."
-	elsif vampire_result == "almost_certainly"
-		puts "Almost certainly a vampire."
-	elsif vampire_result == "prob"
-		puts "Probably a vampire."
-	elsif vampire_result == "prob_not"
-		puts "Probably not a vampire."
-	elsif vampire_result == "inconclusive"
-		puts "Results inconclusive."
-	else
-		puts "Error"
-	end
+# Condition 2: If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+
+if correct_age == false && (garlic_bread == "n" || health_insurance == "n")
+	vampire_status = "probably"
+else
+	#do nothing
 end
 
-vampire_detector(vampire_result)
+# Condition 3: If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+
+if correct_age == false && garlic_bread == "n" && health_insurance == "n"
+	vampire_status = "almost certainly"
+else
+	#do nothing
+end
+
+# Condition 4: Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+
+
+if vampire_name == true
+	vampire_status = "definitely"
+else
+	#do nothing
+end
+
+
+# Condition 5: Otherwise, print “Results inconclusive.”
+
+case vampire_status
+when "definitely"
+	puts "Definitely a vampire."
+when "almost certainly"
+	puts "Almost certainly a vampire."
+when "probably"
+	puts "Probably a vampire."
+when "probably not"
+	puts "Probably not a vampire."
+else
+	puts "Results inconclusive."
+end
