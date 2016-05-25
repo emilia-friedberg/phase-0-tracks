57444cc67f44c744dabc6aa30cc19537780fa65f
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-05-24 18:26:18 -0700

    fix vampire detector with additional if/else/elsif

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,17 +1,21 @@
 puts "What is your name?"
 name = gets.chomp
 
+#testing suspicious names
+if name == "Drake Cula"
+	vampire_name = true
+elsif name == "Tu Fang"
+	vampire_name = true
+else
+	vampire_name = false
+end
+
+
 puts "How old are you?"
 age = gets.chomp.to_i
 
 puts "What year were you born?"
 year_of_birth = gets.chomp.to_i
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic_bread = gets.chomp
-
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-health_insurance = gets.chomp
 
 # defining correct-age boolean variable
 
@@ -23,18 +27,42 @@ else
 	correct_age = false
 end
 
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+health_insurance = gets.chomp
+
+
 # creating vampire detector method
 
-def vampire_detector(name, correct_age, garlic_bread, health_insurance)
-	if correct_age == true && (garlic_bread == "y" || health_insurance == "y")
-		puts "Probably not a vampire."
-	elsif correct_age == false && (garlic_bread == "n" || health_insurance == "n")
+if correct_age == true && (garlic_bread == "y" || health_insurance == "y")
+	vampire_result = "prob_not"
+elsif correct_age == false && (garlic_bread == "n" || health_insurance == "n")
+	vampire_result = "prob"
+elsif correct_age == false && garlic_bread == "n" && health_insurance == "n"
+	vampire_result = "almost_certainly"
+elsif vampire_name == true
+	vampire_result = "definitely"
+else
+	vampire_result = "inconclusive"
+end
+
+def vampire_detector(vampire_result)
+	if vampire_result == "definitely"
+		puts "Definitely a vampire."
+	elsif vampire_result == "almost_certainly"
+		puts "Almost certainly a vampire."
+	elsif vampire_result == "prob"
 		puts "Probably a vampire."
-	elsif correct_age == false && garlic_bread == "n" && health_insurance == "n"
-		puts ""
+	elsif vampire_result == "prob_not"
+		puts "Probably not a vampire."
+	elsif vampire_result == "inconclusive"
+		puts "Results inconclusive."
 	else
-		puts "Not done"
+		puts "Error"
 	end
 end
 
-vampire_detector(name, correct_age, garlic_bread, health_insurance)
+vampire_detector(vampire_result)
