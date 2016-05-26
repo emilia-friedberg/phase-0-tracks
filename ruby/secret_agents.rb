0fc0778fb206aaaf68f06c45d0b08a5447458019
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-05-26 07:14:56 -0700

    fix error with nested method call by adding return str to encrypt and decrypt methods. 4.6 COMPLETE.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -19,9 +19,10 @@ def encrypt(str)
 
 # Print new string
 
- puts str
+ p str
+ return str
+ 
 end
-
 
 # Decrypt
 
@@ -40,8 +41,6 @@ alphabet[new_i]
 
 end
 
-find_previous_letter("j")
-
 def decrypt(str)
  alphabet = "abcdefghijklmnopqrstuvwxyz"
  index = 0
@@ -51,7 +50,48 @@ def decrypt(str)
    index += 1
  end
 
- print str
+ p str
+ return str
 end
 
-decrypt(encrypt("swordfish"))
+# Our Driver Code
+
+# encrypt("abc") should return "bcd"
+
+encrypt("abc")
+
+# encrypt("zed") should return "afe"
+
+encrypt("zed")
+
+# decrypt("bcd") should return "abc"
+
+decrypt("bcd")
+
+# decrypt("afe") should return "zed"
+
+decrypt("afe")
+
+# nested method call
+# this works because the first method called (decrypt) will make each letter in a string the prior one in the alphabet, but it takes as an argument the method encrypt, which advances each letter by one, so they effectively cancel each other out.
+ decrypt(encrypt("swordfish"))
+
+# ask agent if they want to decrypt or encrypt
+
+puts "Would you like to encrypt or decrypt a password?"
+de_or_en = gets.chomp
+
+# ask for password
+puts "Enter your password."
+
+password = gets.chomp
+
+# conduct requested operation and print result
+
+if de_or_en == "decrypt"
+	decrypt(password)
+elsif de_or_en== "encrypt"
+	encrypt(password)
+else
+	puts "Error."
+end
