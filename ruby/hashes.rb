c166c7ead1bf8878815dfdc3766769aa986b1564
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-03 13:53:47 -0400

    add opportunity for user to revise answers and convert answers to correct data type

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -38,3 +38,34 @@ client_preferences = {
 }
 
 p client_preferences
+
+# give user opportunity to revise answers
+puts "Would you like to revise any of these answers?"
+answer = gets.chomp
+
+  if answer == "y" || answer == "yes"
+    puts "Very well. Please type the name of the section, including any underscores, that you would like to change."
+    section_changed = gets.chomp.intern
+    
+# make sure revised answers are the appropriate data type for questions that require integers or Booleans
+
+      if section_changed == :age || section_changed == :number_of_rooms
+        puts "Please type your new answer."
+          new_answer = gets.chomp.to_i
+      elsif section_changed == :has_pets
+        puts "Please type your new answer."
+          new_answer = gets.chomp
+          if new_answer == "true"
+            new_answer = true
+          else
+            new_answer = false
+          end
+      else
+        puts "Please type your new answer."
+          new_answer = gets.chomp
+      end
+    client_preferences[section_changed] = new_answer
+    p client_preferences
+  else
+    puts "Thank you for your input! We look forward to working with you."
+  end
