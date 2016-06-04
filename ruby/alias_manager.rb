f126ae36a9f6fb4f1d65a0c83c1a33685631f3b1
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-04 19:02:03 -0400

    complete release 0 with successfully implemented algorithm

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,30 +1,25 @@
-# swap first and last name:
-  #take full name as parameter and split it into first and last name as an array
-  #reverse the order of the names in the array
-  #put name back together as a string
-  
-def swap_name(full_name)
-  name = full_name.split(' ').to_a
-  reversed_name = name.reverse
-  swapped_name = reversed_name.join(' ')
-  p swapped_name
-end
-
-swap_name("Emilia Friedberg")
-
 # change every vowel to the next vowel, and every consonant to the next consonant
-  # for each letter, check if it is a vowel or a consonant by seeing if it is included in the array of vowels or consonants
-  # if it is a vowel, find its index in the list of vowels, then reassign it to the value of the next index in the list
-  # if it is a consonant, find its index in the list of consonants, then reassign it to the value of the next consonant in the list of consonants
+  # split full name into first and last names
+  # for each letter in the first name, check if it is a vowel or a consonant by seeing if it is included in the array of vowels or consonants
+    # if it is a vowel, find its index in the list of vowels, then reassign it to the value of the next index in the list
+    # if it is a consonant, find its index in the list of consonants, then reassign it to the value of the next consonant in the list of consonants
+    # rejoin letters of name into a string, and capitalize the first letter
+  # repeat above steps for last name
+  # rejoin first and last name in reverse order
   
   
   
-def change_letters(full_name)
+def alias_creator(full_name)
   full_name = full_name.downcase
-  letters = full_name.split('')
+  name = full_name.split(' ').to_a
+  p first_name = name[0]
+  p last_name = name[1]
+  first_name_letters = first_name.split('')
+  last_name_letters = last_name.split('')
   vowels = ["a", "e", "i", "o", "u", "a"]
   consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "b"]
-  letters.map! do |letter|
+  
+  first_name_letters.map! do |letter|
     if vowels.include?(letter)
       letter_index = vowels.index(letter)
       new_letter_index = letter_index + 1
@@ -35,7 +30,24 @@ def change_letters(full_name)
       letter = consonants[new_letter_index]
     end
   end
-  new_name = letters.join('')
+  new_first_name = first_name_letters.join('').capitalize
+  
+  last_name_letters.map! do |letter|
+    if vowels.include?(letter)
+      letter_index = vowels.index(letter)
+      new_letter_index = letter_index + 1
+      letter = vowels[new_letter_index]
+    elsif consonants.include?(letter)
+      letter_index = consonants.index(letter)
+      new_letter_index = letter_index + 1
+      letter = consonants[new_letter_index]
+    end
+  end
+  new_last_name = last_name_letters.join('').capitalize
+
+  new_name = new_last_name + " " + new_first_name
+  p new_name
+  
 end
 
-change_letters("Emilia Friedberg")
+alias_creator("Felicia Torres")
