ac680619e532cc6d6c42f0534ea97f53356e9c14
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-04 16:58:02 -0400

    ignore

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,24 +1,26 @@
-art_gallery {
+art_gallery = {
   school_of_athens: {
+    artist: "Raphael",
     people: [
       "Plato",
       "Aristotle",
       "Heraclitus",
       "Ptolemy",
       "Euclid"
-    ]
+    ],
     objects: [
       'globe',
       'chalkboard',
       'tablet'
     ]
-  }
+  },
   garden_of_earthly_delights: {
+    artist: "Hieronymous Bosch",
     people: [
       "Adam",
       "Eve",
       "guy with the flowers in his bottom",
-    ]
+    ],
     objects: [
       "backgammon board",
       "citadel",
@@ -26,9 +28,10 @@ art_gallery {
       "oversized birds",
       "oversized fruits"
     ]
-  }
+  },
   the_persistence_of_memory: {
-    people: []
+    artist: "Salvador Dali",
+    people: [],
     objects: [
       "melting clock one",
       "melting clock two",
@@ -36,3 +39,16 @@ art_gallery {
     ]
   }
 }
+
+# add Raphael to list of people in the School of Athens
+
+art_gallery[:school_of_athens][:people] << "Raphael"
+
+# capitalize first letter of "guy with the flowers in his bottom"
+
+art_gallery[:garden_of_earthly_delights][:people][2].capitalize
+
+# remove "oversized birds" from list of objects in "the Garden of Earthly Delights"
+
+art_gallery[:garden_of_earthly_delights][:objects].slice!(3)
+p art_gallery[:garden_of_earthly_delights][:objects]
