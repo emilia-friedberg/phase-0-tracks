16cf686ce4a4adaa5b3050b0c39f6c8247b7b2de
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-04 13:01:44 -0400

    iterate through hash using .each

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -17,9 +17,23 @@ p llama_names
 
 # iterate through array with .each
 llama_names.each do |llama_names|
+  puts llama_names
 end
 
 puts "After .each call:"
+p llama_names
+
+puts "Original Names:"
+p llama_names
+
+# iterate through array with .map!
+
+llama_names.map! do |llama_names|
+  puts llama_names
+  llama_names.upcase
+end
+
+puts "After .map! call:"
 p llama_names
 
 # declaring a hash
@@ -31,3 +45,14 @@ llama_ages = {
   Ethan: 1,
   Pete: 5
 }
+
+puts "Original Llama Ages:"
+p llama_ages
+
+# iterate through hash with .each
+llama_ages.each do |llama_ages|
+  puts llama_ages
+end
+
+puts "After .each call:"
+p llama_ages
