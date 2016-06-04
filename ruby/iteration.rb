62c2900fda6871fe3a53d28bf2622b1966f2395a
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-04 13:26:42 -0400

    use documentation to find methods for release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -56,3 +56,94 @@ end
 
 puts "After .each call:"
 p llama_ages
+
+
+# A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
+
+sample_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p sample_array
+p sample_array.reject! { |x| x>7 }
+
+sample_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p sample_array
+p sample_array.delete_if { |x| x>7 }
+
+sample_hash = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7
+}
+p sample_hash
+p sample_hash.reject! { |x, y| y>3 }
+
+
+sample_hash = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7
+}
+p sample_hash
+p sample_hash.delete_if { |x, y| y>3 }
+
+# A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+
+sample_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p sample_array
+p sample_array.keep_if { |x| x>7 }
+
+sample_hash = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7
+}
+p sample_hash
+p sample_hash.keep_if { |x, y| y>3 }
+
+# A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+
+sample_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p sample_array
+p sample_array.select! { |x| x>7 }
+
+sample_hash = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7
+}
+p sample_hash
+p sample_hash.select! { |x, y| y>3 }
+
+
+# A method that will remove items from a data structure until the condition in the block evaluates to false, then stops.
+
+sample_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p sample_array
+p sample_array.take_while{ |x| x<7 }
+
+sample_hash = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7
+}
+p sample_hash
+p sample_hash.take_while { |x, y| y<3 }
