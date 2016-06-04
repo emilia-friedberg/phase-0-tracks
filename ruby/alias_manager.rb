781b57053a90dfa350bcd61567b3b7853ea2ba89
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-04 18:48:16 -0400

    create method to increase each letter in the alphabet

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,3 +11,31 @@ def swap_name(full_name)
 end
 
 swap_name("Emilia Friedberg")
+
+# change every vowel to the next vowel, and every consonant to the next consonant
+  # for each letter, check if it is a vowel or a consonant by seeing if it is included in the array of vowels or consonants
+  # if it is a vowel, find its index in the list of vowels, then reassign it to the value of the next index in the list
+  # if it is a consonant, find its index in the list of consonants, then reassign it to the value of the next consonant in the list of consonants
+  
+  
+  
+def change_letters(full_name)
+  full_name = full_name.downcase
+  letters = full_name.split('')
+  vowels = ["a", "e", "i", "o", "u", "a"]
+  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "b"]
+  letters.map! do |letter|
+    if vowels.include?(letter)
+      letter_index = vowels.index(letter)
+      new_letter_index = letter_index + 1
+      letter = vowels[new_letter_index]
+    elsif consonants.include?(letter)
+      letter_index = consonants.index(letter)
+      new_letter_index = letter_index + 1
+      letter = consonants[new_letter_index]
+    end
+  end
+  new_name = letters.join('')
+end
+
+change_letters("Emilia Friedberg")
