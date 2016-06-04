e934689552c2d302c133a567c7732c9664c431e7
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-04 16:41:10 -0400

    create art gallery hash containing painting hashes containing people and objects in arrays

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,38 @@
+art_gallery {
+  school_of_athens: {
+    people: [
+      "Plato",
+      "Aristotle",
+      "Heraclitus",
+      "Ptolemy",
+      "Euclid"
+    ]
+    objects: [
+      'globe',
+      'chalkboard',
+      'tablet'
+    ]
+  }
+  garden_of_earthly_delights: {
+    people: [
+      "Adam",
+      "Eve",
+      "guy with the flowers in his bottom",
+    ]
+    objects: [
+      "backgammon board",
+      "citadel",
+      "flowers",
+      "oversized birds",
+      "oversized fruits"
+    ]
+  }
+  the_persistence_of_memory: {
+    people: []
+    objects: [
+      "melting clock one",
+      "melting clock two",
+      "melting clock three"
+    ]
+  }
+}
