9a0f1b4f8fd8de302ea7c42bbeab9ad756ee6ad5
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-06 16:01:52 -0400

    add class design to puppy document

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+ANIMAL ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Tail: yes
+Friendly: yes
+Energy: boundless
+Enthusiasm: boundless
+Age: >3 years
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Sniff everything
+Run in circles
+Wag tail
+=end
