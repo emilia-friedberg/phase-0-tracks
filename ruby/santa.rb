7fa8154f02d9d241e89e8087e9f0204280f3d7cb
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-07 16:45:36 -0400

    add celebrate_birthday and get_mad_at methods, getter methods for age and ethnicity, and setter method for gender

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,13 +15,41 @@ class Santa
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
+
+  # getter method for age
+  def age
+    @age
+  end
+
+  # getter method for ethnicity
+  def ethnicity
+    @ethnicity
+  end
+
+  def celebrate_birthday
+    @age = @age += 1
+    # p @age
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking << reindeer_name
+    # p @reindeer_ranking
+  end
+
+  #setter method for gender
+  def gender=(new_gender)
+    @gender = new_gender
+    # p @gender
+  end
+
 end
 
 # driver code
 
-# santa = Santa.new
-# santa.speak
-# santa.eat_milk_and_cookies("double fudge")
+santa = Santa.new("agender", "North Poleian")
+santa.speak
+santa.eat_milk_and_cookies("double fudge")
 
 santas = []
 genders = ["male", "female", "agender", "genderqueer", "gender fluid", "other"]
@@ -34,5 +62,12 @@ end
 santas << Santa.new("female", "Lithuanian")
 santas << Santa.new("other", "Martian")
 santas << Santa.new("gender fluid", "Dominican")
+santas << santa
 
 p santas
+
+santa.celebrate_birthday
+
+santa.get_mad_at("Vixen")
+
+santa.gender=("gender fluid")
