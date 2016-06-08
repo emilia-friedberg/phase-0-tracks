27fe8118983edd6bd1cf66898584882129269fb6
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-08 15:26:41 -0400

    rewrote shout module as mixin and tested by including in two classes and calling module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
   def self.yell_angrily(words)
@@ -13,3 +14,34 @@ end
 #driver code
 p Shout.yell_angrily("This is confusing")
 p Shout.yell_happily("But I have faith that it will make sense soon")
+=end
+
+module Shout
+
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words + "!!!" + " :D"
+  end
+
+end
+
+class Mothers
+  include Shout
+end
+
+class Drivers
+  include Shout
+end
+
+#driver code
+mother = Mothers.new
+driver = Drivers.new
+
+p mother.yell_angrily("Call me sometime")
+p mother.yell_happily("You're coming home")
+
+p driver.yell_angrily("Use your turn signals")
+p driver.yell_happily("I didn't get pulled over for going twenty over")
