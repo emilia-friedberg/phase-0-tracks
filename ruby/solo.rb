af93e0447438505b70ef99bfd5da2a1e291c04e7
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-08 12:42:50 -0400

    add pseudocode for playing cards set

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+=begin
+  
+pseudocode:
+  - define class for playing cards
+  - add initialize method with attributes for number of cards, color of cards, stickiness of cards, and special properties (ie, waterproof)
+  - add method for dealing cards
+    - it takes an integer (the number of players) and divides the number of cards by that integer
+  - add method for throwing cards on the floor
+  - add method for printing the stickiness of the cards
+=end
