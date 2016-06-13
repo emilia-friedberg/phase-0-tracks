4f3e9abcff37876f96641944b55be8c49e293d64
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-13 19:54:51 -0400

    successfully write function to reverse letters in a given string

--- js/explore.js
+++ js/explore.js
@@ -8,16 +8,16 @@
 
 //write a function that takes a string as a parameter and reverses it
 // declare a new empty variable
-// starting from the last letter of the string, add each character to the new variable until each letter has been added
-// print the new variable
+// starting from the last letter of the string, add each character to the new variable until all letters have been added
+// return the new variable
 
 function reverse(str) {
-    var reversedStr = ""
-    for (var i = -1; i > str.length; i--) {
-        reversedStr += str[i]
+    var reversedStr = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
     }
-    console.log(reversedStr);
+    return reversedStr
 }
 
 // driver code:
-reverse("friend")
+reverse("friend");
