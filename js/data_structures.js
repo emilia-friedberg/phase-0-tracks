28faf3d031403d01968c0fa1650edb505b1595a1
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-16 18:10:58 -0400

    add constructor function for cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,26 @@ for (var i = 0; i < horseNames.length; i++) {
 }
 
 // console.log(horses);
+
+
+// constructor function for car
+
+function Car(make, model, year, engineSound) {
+    console.log("Our new car:", this)
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.engineSound = function() { console.log("Vroom vroom!"); };
+}
+
+var myCar = new Car("Kia", "Soul", 2015)
+console.log(myCar)
+myCar.engineSound()
+
+var oldCar = new Car("Hyundai", "Sonata", 2004)
+console.log(oldCar)
+oldCar.engineSound()
+
+var futureCar = new Car("Nasa", "Spaceship", 2035)
+console.log(futureCar)
+futureCar.engineSound()
