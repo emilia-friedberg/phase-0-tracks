35cf33182f7444e092539b7cc9d0abfe3dcf4d3e
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-16 17:55:44 -0400

    create object that assigns colors to horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,5 +4,12 @@ var horseNames = ["Ed", "Juliet", "Concho", "Alexander"];
 horseColors.push("yellow");
 horseNames.push("Wilhelm");
 
-console.log(horseNames)
-console.log(horseColors)
+// console.log(horseNames);
+// console.log(horseColors);
+var horses = {};
+
+for (var i = 0; i < horseNames.length; i++) {
+    horses[horseNames[i]] = horseColors[i];
+}
+
+// console.log(horses);
