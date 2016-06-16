e61610c40598a322eab3e2448a1f506f556b4ae1
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-16 15:54:12 -0400

    create arrays for horse colors and names

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var horseColors = ["tan", "black", "white", "brown"];
+var horseNames = ["Ed", "Juliet", "Concho", "Alexander"];
+
+horseColors.push("yellow");
+horseNames.push("Wilhelm");
+
+console.log(horseNames)
+console.log(horseColors)
