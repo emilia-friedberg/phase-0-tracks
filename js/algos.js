af11295ec1ccd6c0b8c06b06fa8bfe7295c1754a
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-19 17:41:41 -0400

    write function to compare two objects for shared key value pairs

--- js/algos.js
+++ js/algos.js
@@ -1,11 +1,12 @@
 // Find the longest phrase
 // Input: Array of words or phrases
 // Iterate through each value in the array
-//  For each value in the array, take its length, and save it as its value in a new object using the word or phrase as a key
-//  For each value in the array, save its length into a new empty array
-// Sort the array containing the phrase or word lengths by length
-// Search the object (containing the words or phrases paired with their corresponding lengths) for the word or phrase that corresponds to the longest length
-// Print that word or phrase
+//  FOR EACH value in the array, take its length, and SAVE it as its value in a new object using the word or phrase as a key
+//  FOR EACH value in the array, SAVE its length into a new empty array
+// SORT the array containing the phrase or word lengths by length
+// SEARCH the object (containing the words or phrases paired with their corresponding lengths) for the word or phrase that corresponds to the longest length
+// PRINT that word or phrase
+// Output: longest word or phrase
 
 
 function longestPhraseFinder(array) {
@@ -26,8 +27,35 @@ function longestPhraseFinder(array) {
   }
 }
 
+
+// Compare two objects for shared key-value pair
+// Input: two objects
+// Iterate through first object to see:
+//  IF a key in the first object matches the value of a key in the second object, AND  the values of those respective keys match:
+//    THEN it is true that the objects share at least one key-value pair.
+//  OTHERWISE
+//    It is not true that the objects share at least one key-value pair.
+// Output: boolean
+
+
+function objectMatcher(object1, object2) {
+  var object1Keys = [];
+  var keyValueMatch = false;
+  for (var key in object1) {
+    if (object1.hasOwnProperty(key) && object2.hasOwnProperty(key) && (object1[key] === object2[key])) {
+      keyValueMatch = true;
+    }
+  }
+  console.log(keyValueMatch);
+}
+
 // driver code
-longestPhraseFinder(["long phrase", "longest phrase", "longer phrase"]);
-longestPhraseFinder(["supercallifragilistic", "supercall", "supercallifragilisticexpealladocious", "expeallodocious", "super"]);
-longestPhraseFinder(["short", "even shorter", "absolute shortest", "the preceding statements are false"]);
-longestPhraseFinder(["this", "is", "small"])
+// longestPhraseFinder(["long phrase", "longest phrase", "longer phrase"]);
+// longestPhraseFinder(["supercallifragilistic", "supercall", "supercallifragilisticexpealladocious", "expeallodocious", "super"]);
+// longestPhraseFinder(["short", "even shorter", "absolute shortest", "the preceding statements are false"]);
+// longestPhraseFinder(["this", "is", "small"])
+objectMatcher({ name: "Steven", age: 54 }, { name: "Tamir", age: 54 });
+objectMatcher({ name: "Steven", age: 54 }, { name: "Tamir", age: 17 });
+objectMatcher({ food: "olives", flavor: "briny" }, { food: "pickles", flavor: "briny" });
+objectMatcher({ food: "olives", flavor: "briny" }, { food: "candy", flavor: "sweet" });
+objectMatcher({ key: "value", key2: "value2", key3: "value3" }, { key4: "value4", key5: "value3" })
