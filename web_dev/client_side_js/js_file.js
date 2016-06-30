5693d58899fe4fa511c112e55f12a80b3c4ac03b
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-06-30 15:51:28 -0400

    add event listener message to page using js manipulation

--- web_dev/client_side_js/js_file.js
+++ web_dev/client_side_js/js_file.js
@@ -2,3 +2,9 @@
 
 var paragraph1 = document.getElementById("paragraph1");
 paragraph1.insertAdjacentHTML('beforebegin', '<h1>Header</h1>');
+
+// adding an event listener to the page
+
+paragraph1.addEventListener('mouseover', function() {
+  console.log('You moused over!');
+});
